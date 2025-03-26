0388e02eae2a68580388632940124b2ca639e752
Alexandre Lupi <alelupi136@gmail.com>
2025-03-26 20:19:24 -0300

    Commit - 3 - JS incluido

---
+++ script/index.js
@@ -0,0 +1,11 @@
+class Calculator{
+
+    constructor(){
+        this.upperValue = document .querySelector('#upper-number');
+        this.resultValue = document.querySelector('#result-number');
+        this.reset = 0;
+    }
+}
+
+let buttons = document.querySelectorAll('.btn');
+console.log(buttons);
