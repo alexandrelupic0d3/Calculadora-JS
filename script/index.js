d5f29a44a90a30a868a447ca01f40c1a3f71605f
Alexandre Lupi <alelupi136@gmail.com>
2025-03-26 20:33:35 -0300

    Commit - 4 - fez a resolution

--- script/index.js
+++ script/index.js
@@ -5,7 +5,18 @@ class Calculator{
         this.resultValue = document.querySelector('#result-number');
         this.reset = 0;
     }
+    //metodo de soma
+        sum(n1,n2){
+            return = parseFloat=(n1) + parseFloat (n2)
+        }
+
+resolution(){
+    //nós jogamos parauma array uma string
+    let upperValuearray =  (this.upperValue.textContent).split("  ");
+    result = Calc.sum(upperValuearray[i - 1], upperValuearray[i + 1]);
 }
 
+}
+//start no btns
 let buttons = document.querySelectorAll('.btn');
 console.log(buttons);
